67ae469029fad67ac3fcab4bbe257833d76aead4
geici <geicianecosta43@gmail.com>
2022-10-29 14:42:22 -0400

    added logic for the description of the mode

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import { React, useState, useEffect, createContext } from 'react';
 import ReactSwitch from 'react-switch';
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
+
 function App() {
 
   const [theme, setTheme] = useState('dark')
-
+  const [mode, setMode] = useState('')
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+    setTheme((curr) => (curr === 'light' ? 'dark'
+      : 'light'))
+
   };
+  useEffect(() => {
+    if (theme === 'dark') {
+      setMode('dark')
+    }
+
+    console.log(theme)
+    if (theme === 'light') {
+      setMode('light')
+    }
+  }, [theme])
 
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
+
+
       <div className="App" id={theme}>
-        <div className='switch-container'>
-      <div className='switch'>
-          <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'} />
-        </div>
-        </div>
+        <header>
+
+          <h1>Weather Dashboard</h1>
+
+          <div className='switch'>
+            {/* <span className='theme-description'>Mode</span> */}
+            <span className='theme-description'>{mode} Mode</span>
+            <ReactSwitch
+
+
+              onChange={toggleTheme} checked={theme === 'dark'} />
+          </div>
+        </header>
+
+
         <div className='test'>
           test
         </div>
       </div>
-    </ThemeContext.Provider>
+    </ThemeContext.Provider >
   );
 }
 
---
+++ src/components/CardList/Cardlist.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { React, useState, useEffect, createContext } from 'react';
+
+const Cardlist = ()=>{
+return (
+    <h1>test</h1>
+)
+}
+
+export default Cardlist;
